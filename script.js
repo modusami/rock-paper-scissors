4463a98a8cc08d4c219c5e90c82e42fd616687e1
odusami <modusami03@gmail.com>
2023-06-28 19:52:33 -0400

    starting files

---
+++ script.js
@@ -0,0 +1,6 @@
+const rockButton = document.querySelector(".rock");
+const paperButton = document.querySelector(".paper");
+const scissorsButton = document.querySelector(".scissors");
+const arr = [rockButton, paperButton, scissorsButton];
+
+
