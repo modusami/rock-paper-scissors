95c29d8957e5dc08e7578c33ef08aa85d020ca19
odusami <modusami03@gmail.com>
2023-06-28 20:08:02 -0400

    Establish Computer And Plyaer Choice

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const rockButton = document.querySelector(".rock");
 const paperButton = document.querySelector(".paper");
 const scissorsButton = document.querySelector(".scissors");
-const arr = [rockButton, paperButton, scissorsButton];
+let playerChoice = null;
+const symbolRock = "rock", symbolPaper = "paper", symbolScissors = "scissors";
+
+rockButton.addEventListener("click", ()=>{
+    playerChoice = symbolRock;
+});
+
+paperButton.addEventListener("click", ()=>{
+    playerChoice = symbolPaper;
+});
+
+scissorsButton.addEventListener("click", ()=>{
+    playerChoice = symbolScissors;
+});
+
+function getComputerChoice()
+{
+    const choices = [symbolRock, symbolPaper, symbolScissors];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function determineWinner()
+{
+    
+}
 
 
